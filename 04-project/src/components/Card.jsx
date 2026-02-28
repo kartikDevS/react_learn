d7a78bdff30529199d64675f3016f6680c9c8208
kartikDevS <kartik9136@gmail.com>
2026-02-28 12:19:07 +0530

    cards-project

--- 04-project/src/components/Card.jsx
+++ 04-project/src/components/Card.jsx
@@ -1,36 +1,31 @@
-import React from 'react'
 import { Bookmark } from 'lucide-react'
 const Card = (props) => {
-  return (
-    <div class='card'>
-      <img src={props.img} alt=""></img>
-      <button>Save <Bookmark /></button>
-      <div class="company">
-        <p id="company-name">{props.company}</p>
-        <p id="days">{props.days} days ago</p>
-      </div>
-      <div id="role">
-        <h4>{props.role}</h4>
-      </div>
-      <div class="time1">
-        <p>{props.time1}</p>
-      </div>
-      <div class="time2">
-        <p>{props.time2}</p>
-      </div>
-      <div class="line">
-      </div>
-      <div class="price">
-        {props.price}$/hr
-      </div>
-      <div class="apply">
-        <button>Apply now</button>
-      </div>
-      <div class="place">
-        {props.city}, {props.country}
-      </div>
-    </div>
-  )
+
+    return (
+        <div className="card">
+            <div>
+                <div className="top">
+                    <img src={props.brandLogo} alt="nachioooooo" />
+                    <button>Save <Bookmark size={10} /> </button>
+                </div>
+                <div className="center">
+                    <h3>{props.company} <span>{props.datePosted}</span></h3>
+                    <h2>{props.post}</h2>
+                    <div className='tag'>
+                        <h4>{props.tag1}</h4>
+                        <h4>{props.tag2}</h4>
+                    </div>
+                </div>
+            </div>
+            <div className="bottom">
+                <div>
+                    <h3>{props.pay}</h3>
+                    <p>Mumbai, India</p>
+                </div>
+                <button>Apply Now</button>
+            </div>
+        </div>
+    )
 }
 
 export default Card
